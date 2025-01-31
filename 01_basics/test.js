f4de678e23ecc7424b077311e0528bf66085699d
DebadityaMukherjee1 <debadityamukherjee1@gmail.com>
2025-01-31 23:17:43 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,9 @@
 console.log("hello")
+const accountID = 12345
+let email = "debadityamukherjee1@gmail.com"
+let personname = "Debaditya Mukherjee"
+console.log(accountID)
+console.log(email)
+console.log(personname)
+console.table([accountID, email, personname])
+console.log(typeof accountID)
